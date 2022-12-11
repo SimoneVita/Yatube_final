58d15da2c66828a7a500c3fd3c21d708d7f12481
Simonenko Vitaly <vitalysimonenko@yandex.ru>
2022-12-11 03:50:38 +0300

    Second iteration 1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -134,6 +134,7 @@ def follow_index(request):
     }
     return render(request, 'posts/follow.html', context)
 
+
 @login_required
 def profile_follow(request, username):
     user = request.user
@@ -142,6 +143,7 @@ def profile_follow(request, username):
         Follow.objects.get_or_create(user=user, author=author)
         return redirect('posts:profile', username=author)
     return redirect('posts:profile', username=author)
+
 
 @login_required
 def profile_unfollow(request, username):
